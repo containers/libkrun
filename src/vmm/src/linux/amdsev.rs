7098deb05eaca9c16787c92c249969951f19d4d8
Sergio Lopez <slp@redhat.com>
2022-08-17 12:32:17 +0200

    sev: replace ureq with curl
    Replace the use of ureq crate with curl, which is easier to find
    packaged in distros.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- src/vmm/src/linux/amdsev.rs
+++ src/vmm/src/linux/amdsev.rs
@@ -1,15 +1,16 @@
 use std::fmt;
 use std::fs::File;
+use std::io::Read;
 use std::mem::{size_of_val, MaybeUninit};
 use std::os::unix::io::AsRawFd;
 use std::path::Path;
-use std::sync::Arc;
-use std::time::Duration;
+use std::sync::{Arc, Mutex};
 
 use super::super::resources::TeeConfig;
 use super::vstate::MeasuredRegion;
 
 use codicon::{Decoder, Encoder};
+use curl::easy::{Easy, List};
 use kbs_types::{Attestation, Challenge, Request, SevChallenge, SevRequest, Tee, TeePubKey};
 use kvm_bindings::{kvm_enc_region, kvm_sev_cmd};
 use kvm_ioctls::VmFd;
@@ -23,12 +24,12 @@ use vm_memory::{GuestAddress, GuestMemory, GuestMemoryMmap, GuestMemoryRegion};
 
 #[derive(Debug)]
 pub enum Error {
-    AttestationRequest(ureq::Error),
+    AttestationRequest(curl::Error),
     DecodeAskArk,
     DecodeCek,
     DecodeChain,
-    DownloadCek,
-    DownloadAskArk,
+    DownloadCek(curl::Error),
+    DownloadAskArk(curl::Error),
     EncodeChain,
     FetchIdentifier,
     InvalidCpuData,
@@ -43,7 +44,7 @@ pub enum Error {
     ReadingCpuData(procfs::ProcError),
     ReadingCoreData,
     SessionFromPolicy(std::io::Error),
-    SessionRequest(ureq::Error),
+    SessionRequest(curl::Error),
     SevInit(kvm_ioctls::Error),
     SevInjectSecret(kvm_ioctls::Error),
     SevLaunchFinish(kvm_ioctls::Error),
@@ -53,6 +54,93 @@ pub enum Error {
     SevLaunchUpdateVmsa(kvm_ioctls::Error),
     StartFromSession(std::io::Error),
     UnknownCpuModel,
+}
+
+struct CurlAgent {
+    easy: Easy,
+    session_id: Option<String>,
+}
+
+fn extract_session_id(header: &[u8]) -> Option<String> {
+    let header = match std::str::from_utf8(header) {
+        Ok(h) => h,
+        Err(_) => return None,
+    };
+
+    if !header.contains("session_id") {
+        return None;
+    }
+
+    let parts: Vec<&str> = header.split(';').collect();
+    for p in parts {
+        let elems: Vec<&str> = p.split('=').collect();
+        if elems.len() == 2 && elems[0].contains("session_id") {
+            return Some(elems[1].to_string());
+        }
+    }
+
+    None
+}
+
+impl CurlAgent {
+    fn new() -> Self {
+        CurlAgent {
+            easy: Easy::new(),
+            session_id: None,
+        }
+    }
+
+    fn get(&mut self, url: &str) -> Result<Vec<u8>, curl::Error> {
+        let mut rsp = Vec::new();
+
+        self.easy.post(false)?;
+        self.easy.url(url)?;
+
+        let mut transfer = self.easy.transfer();
+        transfer.write_function(|data| {
+            rsp.extend_from_slice(data);
+            Ok(data.len())
+        })?;
+        transfer.perform()?;
+        drop(transfer);
+
+        Ok(rsp)
+    }
+
+    fn post(&mut self, url: &str, mut data: &[u8]) -> Result<Vec<u8>, curl::Error> {
+        let mut rsp = Vec::new();
+
+        let mut headers = List::new();
+        headers.append("Accept: application/json")?;
+        headers.append("Content-Type: application/json; charset=utf-8")?;
+        if let Some(session_id) = &self.session_id {
+            headers.append(&format!("Cookie: session_id={}", session_id))?;
+        }
+
+        self.easy.post(true)?;
+        self.easy.post_field_size(data.len() as u64)?;
+        self.easy.url(url)?;
+        self.easy.http_headers(headers)?;
+
+        let mut transfer = self.easy.transfer();
+        transfer.read_function(|buf| Ok(data.read(buf).unwrap_or(0)))?;
+        transfer.write_function(|data| {
+            rsp.extend_from_slice(data);
+            Ok(data.len())
+        })?;
+        transfer
+            .header_function(|header| {
+                if let Some(session_id) = extract_session_id(header) {
+                    self.session_id = Some(session_id);
+                }
+                true
+            })
+            .unwrap();
+        transfer.perform()?;
+        drop(transfer);
+
+        Ok(rsp)
+    }
 }
 
 enum CpuModel {
@@ -102,7 +190,7 @@ fn find_cpu_model() -> Result<CpuModel, Error> {
     }
 }
 
-fn fetch_chain(fw: &mut Firmware) -> Result<certs::Chain, Error> {
+fn fetch_chain(fw: &mut Firmware, curl_agent: &mut CurlAgent) -> Result<certs::Chain, Error> {
     const CEK_SVC: &str = "https://kdsintf.amd.com/cek/id";
     const ASK_ARK_SVC: &str = "https://developer.amd.com/wp-content/resources/";
 
@@ -111,24 +199,22 @@ fn fetch_chain(fw: &mut Firmware) -> Result<certs::Chain, Error> {
         .expect("unable to export SEV certificates");
 
     let id = fw.get_identifier().map_err(|_| Error::FetchIdentifier)?;
-    let url = format!("{}/{}", CEK_SVC, id);
 
-    let mut rsp = ureq::get(&url)
-        .call()
-        .map_err(|_| Error::DownloadCek)?
-        .into_reader();
+    let rsp = curl_agent
+        .get(&format!("{}/{}", CEK_SVC, id))
+        .map_err(Error::DownloadCek)?;
 
-    chain.cek = (certs::sev::Certificate::decode(&mut rsp, ())).map_err(|_| Error::DecodeCek)?;
+    chain.cek =
+        (certs::sev::Certificate::decode(&mut rsp.as_slice(), ())).map_err(|_| Error::DecodeCek)?;
 
     let cpu_model = find_cpu_model()?;
-    let url = format!("{}/ask_ark_{}.cert", ASK_ARK_SVC, cpu_model);
-    let mut rsp = ureq::get(&url)
-        .call()
-        .map_err(|_| Error::DownloadAskArk)?
-        .into_reader();
+
+    let rsp = curl_agent
+        .get(&format!("{}/ask_ark_{}.cert", ASK_ARK_SVC, cpu_model))
+        .map_err(Error::DownloadCek)?;
 
     Ok(certs::Chain {
-        ca: certs::ca::Chain::decode(&mut rsp, ()).map_err(|_| Error::DecodeAskArk)?,
+        ca: certs::ca::Chain::decode(&mut rsp.as_slice(), ()).map_err(|_| Error::DecodeAskArk)?,
         sev: chain,
     })
 }
@@ -139,7 +225,11 @@ struct SevCertConfig {
     pub attestation_server_pubkey: String,
 }
 
-fn get_and_store_chain(fw: &mut Firmware, tee_config: &TeeConfig) -> Result<certs::Chain, Error> {
+fn get_and_store_chain(
+    fw: &mut Firmware,
+    tee_config: &TeeConfig,
+    curl_agent: &mut CurlAgent,
+) -> Result<certs::Chain, Error> {
     let cert_config: SevCertConfig =
         serde_json::from_str(&tee_config.tee_data).map_err(Error::ParseSevCertConfig)?;
 
@@ -148,7 +238,7 @@ fn get_and_store_chain(fw: &mut Firmware, tee_config: &TeeConfig) -> Result<cert
         let mut file = File::open(filepath).map_err(Error::OpenChainFile)?;
         Ok(certs::Chain::decode(&mut file, ()).map_err(|_| Error::DecodeChain)?)
     } else {
-        let chain = fetch_chain(fw)?;
+        let chain = fetch_chain(fw, curl_agent)?;
         let mut file = File::create("/tmp/libkrun-sev.chain").map_err(|_| Error::OpenTmpFile)?;
         chain
             .encode(&mut file, ())
@@ -177,16 +267,17 @@ pub struct AmdSev {
     start: Start,
     session_id: Option<String>,
     sev_es: bool,
-    ureq_agent: Option<Arc<ureq::Agent>>,
+    curl_agent: Arc<Mutex<CurlAgent>>,
 }
 
 impl AmdSev {
     pub fn new(tee_config: &TeeConfig) -> Result<Self, Error> {
         let mut fw = Firmware::open().map_err(Error::OpenFirmware)?;
-        let chain = get_and_store_chain(&mut fw, tee_config)?;
+        let mut curl_agent = CurlAgent::new();
+        let chain = get_and_store_chain(&mut fw, tee_config, &mut curl_agent)?;
         let mut sev_es = false;
 
-        let (start, session_id, ureq_agent) = if !tee_config.attestation_url.is_empty() {
+        let (start, session_id) = if !tee_config.attestation_url.is_empty() {
             let build = fw
                 .platform_status()
                 .map_err(|_| Error::PlatformStatus)?
@@ -197,23 +288,18 @@ impl AmdSev {
                 version: "0.0.0".to_string(),
                 workload_id: tee_config.workload_id.clone(),
                 tee: tee_config.tee.clone(),
-                extra_params: ureq::json!(sev_request).to_string(),
+                extra_params: serde_json::json!(sev_request).to_string(),
             };
 
-            let ureq_agent = ureq::AgentBuilder::new()
-                .timeout_read(Duration::from_secs(5))
-                .timeout_write(Duration::from_secs(5))
-                .build();
-
-            let response = ureq_agent
-                .post(format!("{}/kbs/v0/auth", tee_config.attestation_url).as_str())
-                .send_json(ureq::json!(request))
-                .map_err(Error::SessionRequest)?
-                .into_string()
-                .unwrap();
+            let response = curl_agent
+                .post(
+                    format!("{}/kbs/v0/auth", tee_config.attestation_url).as_str(),
+                    serde_json::json!(request).to_string().as_bytes(),
+                )
+                .map_err(Error::SessionRequest)?;
 
             let challenge: Challenge =
-                serde_json::from_str(&response).map_err(Error::ParseSessionResponse)?;
+                serde_json::from_slice(&response).map_err(Error::ParseSessionResponse)?;
             let sev_challenge: SevChallenge = serde_json::from_str(&challenge.extra_params)
                 .map_err(Error::ParseSessionResponse)?;
 
@@ -226,19 +312,11 @@ impl AmdSev {
                 sev_es = true;
             }
 
-            (
-                sev_challenge.start,
-                Some(sev_challenge.id),
-                Some(Arc::new(ureq_agent)),
-            )
+            (sev_challenge.start, Some(sev_challenge.id))
         } else {
             let policy = Policy::default();
             let session = Session::try_from(policy).map_err(Error::SessionFromPolicy)?;
-            (
-                session.start(chain).map_err(Error::StartFromSession)?,
-                None,
-                None,
-            )
+            (session.start(chain).map_err(Error::StartFromSession)?, None)
         };
 
         Ok(AmdSev {
@@ -247,7 +325,7 @@ impl AmdSev {
             start,
             session_id,
             sev_es,
-            ureq_agent,
+            curl_agent: Arc::new(Mutex::new(curl_agent)),
         })
     }
 
@@ -456,30 +534,26 @@ impl AmdSev {
                 nonce: self.session_id.as_ref().unwrap().clone(),
                 tee: Tee::Sev,
                 tee_pubkey,
-                tee_evidence: ureq::json!(measurement).to_string(),
+                tee_evidence: serde_json::json!(measurement).to_string(),
             };
 
-            let ureq_agent = self.ureq_agent.as_ref().unwrap().clone();
-            ureq_agent
-                .post(&format!(
-                    "{}/kbs/v0/attest",
-                    self.tee_config.attestation_url,
-                ))
-                .send_json(ureq::json!(attestation))
+            let mut curl_agent = self.curl_agent.lock().unwrap();
+            curl_agent
+                .post(
+                    &format!("{}/kbs/v0/attest", self.tee_config.attestation_url,),
+                    serde_json::json!(attestation).to_string().as_bytes(),
+                )
                 .map_err(Error::AttestationRequest)?;
 
-            let secret_resp = ureq_agent
+            let secret_resp = curl_agent
                 .get(&format!(
                     "{}/kbs/v0/key/{}",
                     self.tee_config.attestation_url, self.tee_config.workload_id,
                 ))
-                .call()
-                .map_err(Error::AttestationRequest)?
-                .into_string()
-                .unwrap();
+                .map_err(Error::AttestationRequest)?;
 
             let secret: Secret =
-                serde_json::from_str(&secret_resp).map_err(Error::ParseAttestationSecret)?;
+                serde_json::from_slice(&secret_resp).map_err(Error::ParseAttestationSecret)?;
 
             let secret_host_addr = guest_mem
                 .get_host_address(GuestAddress(arch::x86_64::layout::CMDLINE_START))
