2130ce631441e8b60cbdfd2dd64f7fdcbdd9ed30
Sergio Lopez <slp@redhat.com>
2022-11-14 10:37:58 -0500

    snp: refactor code to add_region
    Move the code the add a measured region to a dedicated method.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- src/vmm/src/linux/tee/amdsnp.rs
+++ src/vmm/src/linux/tee/amdsnp.rs
@@ -69,65 +69,69 @@ impl AmdSnp {
         Ok(launcher)
     }
 
+    fn add_region(
+        &self,
+        guest_mem: &GuestMemoryMmap,
+        region: MeasuredRegion,
+        launcher: &mut Launcher<Started, RawFd, RawFd>,
+        page_type: PageType,
+    ) -> Result<(), Error> {
+        let dp = VmplPerms::empty();
+        let ga = GuestAddress(region.guest_addr);
+
+        /*
+         * Use the guest's address to obtain its GuestRegionMmap, and then
+         * convert this region to a slice. Basically, we are taking an
+         * entire slice of a guest memory region.
+         */
+        let gr: &GuestRegionMmap = guest_mem.find_region(ga).unwrap();
+        let region_slice = unsafe { gr.as_slice().unwrap() };
+
+        /*
+         * The memory region we are currently looking to measure is
+         * represented simply as a guest address at the moment. Instead,
+         * we would like to obtain a slice of it. To do this, we must use
+         * the guest address as an OFFSET within the slice, and only take
+         * that subslice.
+         */
+        let offset: usize = guest_mem
+            .to_region_addr(ga)
+            .unwrap()
+            .1
+             .0
+            .try_into()
+            .unwrap();
+
+        /*
+         * We know the size of the region to be measured from the
+         * MeasuredRegion.
+         */
+        let count: usize = region.size;
+
+        /*
+         * We now have the start and end indexes of the slice, so use these
+         * indexes to take a subslice of the guest region (corresponding to
+         * the slice of bytes that we're looking to measure).
+         */
+        let buf = &region_slice[offset..offset + count];
+
+        /*
+         * From that subslice, build an Update struct and call
+         * SNP_LAUNCH_UPDATE.
+         */
+        let update = Update::new(region.guest_addr >> 12, buf, false, page_type, (dp, dp, dp));
+
+        launcher.update_data(update).map_err(Error::LaunchUpdate)
+    }
+
     pub fn vm_measure(
         &self,
         guest_mem: &GuestMemoryMmap,
         measured_regions: Vec<MeasuredRegion>,
         mut launcher: Launcher<Started, RawFd, RawFd>,
     ) -> Result<(), Error> {
-        let dp = VmplPerms::empty();
         for region in measured_regions {
-            let page_type = if region.guest_addr == arch::x86_64::layout::ZERO_PAGE_START {
-                PageType::Zero
-            } else {
-                PageType::Normal
-            };
-
-            let ga = GuestAddress(region.guest_addr);
-
-            /*
-             * Use the guest's address to obtain its GuestRegionMmap, and then
-             * convert this region to a slice. Basically, we are taking an
-             * entire slice of a guest memory region.
-             */
-            let gr: &GuestRegionMmap = guest_mem.find_region(ga).unwrap();
-            let region_slice = unsafe { gr.as_slice().unwrap() };
-
-            /*
-             * The memory region we are currently looking to measure is
-             * represented simply as a guest address at the moment. Instead,
-             * we would like to obtain a slice of it. To do this, we must use
-             * the guest address as an OFFSET within the slice, and only take
-             * that subslice.
-             */
-            let offset: usize = guest_mem
-                .to_region_addr(ga)
-                .unwrap()
-                .1
-                 .0
-                .try_into()
-                .unwrap();
-
-            /*
-             * We know the size of the region to be measured from the
-             * MeasuredRegion.
-             */
-            let count: usize = region.size;
-
-            /*
-             * We now have the start and end indexes of the slice, so use these
-             * indexes to take a subslice of the guest region (corresponding to
-             * the slice of bytes that we're looking to measure).
-             */
-            let buf = &region_slice[offset..offset + count];
-
-            /*
-             * From that subslice, build an Update struct and call
-             * SNP_LAUNCH_UPDATE.
-             */
-            let update = Update::new(region.guest_addr >> 12, buf, false, page_type, (dp, dp, dp));
-
-            launcher.update_data(update).map_err(Error::LaunchUpdate)?;
+            self.add_region(guest_mem, region, &mut launcher, PageType::Normal)?;
         }
 
         let finish = Finish::new(None, None, [0; 32]);
