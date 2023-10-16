e6dc894ca84e606277e526c32b81dc184a2214a4
Tyler Fanelli <tfanelli@redhat.com>
2023-10-16 19:33:43 -0400

    devices/bus: Change PartialOrd implementation
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/devices/src/bus.rs
+++ src/devices/src/bus.rs
@@ -69,7 +69,7 @@ impl Ord for BusRange {
 
 impl PartialOrd for BusRange {
     fn partial_cmp(&self, other: &BusRange) -> Option<Ordering> {
-        self.0.partial_cmp(&other.0)
+        Some(self.cmp(other))
     }
 }
 
