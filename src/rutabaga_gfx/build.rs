41a6fe635706260d5557f9c5f7679872eef988fc
Sergio Lopez <slp@redhat.com>
2024-02-15 15:41:50 +0100

    rutabaga: gate pkg_config checks behind "gpu"
    Crates are loaded even when they are optional and not actually used,
    which means that "rutabaga_gfx/build.rs" is executed even when not
    enabling the "gpu" feature. This would force us to install epoxy and
    virglrenderer in contexts where we don't really need them. Instead,
    let's gate the pkg_config checks behind the "gpu" feature.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- src/rutabaga_gfx/build.rs
+++ src/rutabaga_gfx/build.rs
@@ -11,10 +11,13 @@ fn main() -> Result<(), pkg_config::Error> {
         return Ok(());
     }
 
-    pkg_config::Config::new().probe("epoxy")?;
-    #[cfg(target_os = "linux")]
-    pkg_config::Config::new().probe("libdrm")?;
-    pkg_config::Config::new().probe("virglrenderer")?;
+    #[cfg(feature = "gpu")]
+    {
+        pkg_config::Config::new().probe("epoxy")?;
+        #[cfg(target_os = "linux")]
+        pkg_config::Config::new().probe("libdrm")?;
+        pkg_config::Config::new().probe("virglrenderer")?;
+    }
 
     Ok(())
 }
