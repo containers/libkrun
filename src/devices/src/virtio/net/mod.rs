62b48cbce7e939daf878dd67f4c753e893f56c67
Sergio Lopez <slp@redhat.com>
2024-02-26 13:43:39 +0100

    virtio/net: increase queue size to 1024
    Increase queue size to 1024. This minimizes the chances of dropping
    incoming packets.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- src/devices/src/virtio/net/mod.rs
+++ src/devices/src/virtio/net/mod.rs
@@ -3,7 +3,7 @@
 
 use std::{io, result};
 pub const MAX_BUFFER_SIZE: usize = 65562;
-pub const QUEUE_SIZE: u16 = 128;
+pub const QUEUE_SIZE: u16 = 1024;
 pub const NUM_QUEUES: usize = 2;
 pub const QUEUE_SIZES: &[u16] = &[QUEUE_SIZE; NUM_QUEUES];
 // The index of the rx queue from Net device queues/queues_evts vector.
