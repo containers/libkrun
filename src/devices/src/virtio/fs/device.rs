containers/libkrun

commit eac94787bab275ab3611a339129c46035e6f47a4

virtio/fs: move work to a separate thread
Move the work to a separate thread. This reduces the operation latency,
minimizes the noise between devices and allows us to implement more
aggressive optimization strategies.

Signed-off-by: Sergio Lopez <slp@redhat.com>

src/devices/src/virtio/fs/device.rs
@@ -1,29 +1,20 @@
 use std::cmp;
 use std::io::Write;
-use std::result;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::atomic::AtomicUsize;
 use std::sync::{Arc, Mutex};
+use std::thread::JoinHandle;
 
-use utils::eventfd::EventFd;
+use utils::eventfd::{EventFd, EFD_NONBLOCK};
+use virtio_bindings::{virtio_config::VIRTIO_F_VERSION_1, virtio_ring::VIRTIO_RING_F_EVENT_IDX};
 use vm_memory::{ByteValued, GuestMemoryMmap};
 
 use super::super::{
-    ActivateError, ActivateResult, DeviceState, FsError, Queue as VirtQueue, VirtioDevice,
-    VirtioShmRegion, VIRTIO_MMIO_INT_VRING,
+    ActivateResult, DeviceState, FsError, Queue as VirtQueue, VirtioDevice, VirtioShmRegion,
 };
-use super::descriptor_utils::{Reader, Writer};
-use super::passthrough::{self, PassthroughFs};
-use super::server::Server;
+use super::passthrough;
+use super::worker::FsWorker;
 use super::{defs, defs::uapi};
 use crate::legacy::Gic;
-use crate::Error as DeviceError;
-
-// High priority queue.
-pub(crate) const HPQ_INDEX: usize = 0;
-// Request queue.
-pub(crate) const REQ_INDEX: usize = 1;
-
-pub(crate) const AVAIL_FEATURES: u64 = 1 << uapi::VIRTIO_F_VERSION_1 as u64;
 
 #[derive(Copy, Clone)]
 #[repr(C, packed)]
@@ -44,19 +35,20 @@ impl Default for VirtioFsConfig {
 unsafe impl ByteValued for VirtioFsConfig {}
 
 pub struct Fs {
-    pub(crate) queues: Vec<VirtQueue>,
-    pub(crate) queue_events: Vec<EventFd>,
-    pub(crate) avail_features: u64,
-    pub(crate) acked_features: u64,
-    pub(crate) interrupt_status: Arc<AtomicUsize>,
-    pub(crate) interrupt_evt: EventFd,
-    pub(crate) activate_evt: EventFd,
-    pub(crate) device_state: DeviceState,
-    config: VirtioFsConfig,
-    shm_region: Option<VirtioShmRegion>,
-    server: Server<PassthroughFs>,
+    queues: Vec<VirtQueue>,
+    queue_events: Vec<EventFd>,
+    avail_features: u64,
+    acked_features: u64,
+    interrupt_status: Arc<AtomicUsize>,
+    interrupt_evt: EventFd,
     intc: Option<Arc<Mutex<Gic>>>,
     irq_line: Option<u32>,
+    device_state: DeviceState,
+    config: VirtioFsConfig,
+    shm_region: Option<VirtioShmRegion>,
+    passthrough_cfg: passthrough::Config,
+    worker_thread: Option<JoinHandle<()>>,
+    worker_stopfd: EventFd,
 }
 
 impl Fs {
@@ -71,6 +63,8 @@ impl Fs {
                 .push(EventFd::new(utils::eventfd::EFD_NONBLOCK).map_err(FsError::EventFd)?);
         }
 
+        let avail_features = (1u64 << VIRTIO_F_VERSION_1) | (1u64 << VIRTIO_RING_F_EVENT_IDX);
+
         let tag = fs_id.into_bytes();
         let mut config = VirtioFsConfig::default();
         config.tag[..tag.len()].copy_from_slice(tag.as_slice());
@@ -84,17 +78,18 @@ impl Fs {
         Ok(Fs {
             queues,
             queue_events,
-            avail_features: AVAIL_FEATURES,
+            avail_features,
             acked_features: 0,
             interrupt_status: Arc::new(AtomicUsize::new(0)),
             interrupt_evt: EventFd::new(utils::eventfd::EFD_NONBLOCK).map_err(FsError::EventFd)?,
-            activate_evt: EventFd::new(utils::eventfd::EFD_NONBLOCK).map_err(FsError::EventFd)?,
+            intc: None,
+            irq_line: None,
             device_state: DeviceState::Inactive,
             config,
             shm_region: None,
-            server: Server::new(PassthroughFs::new(fs_cfg).unwrap()),
-            intc: None,
-            irq_line: None,
+            passthrough_cfg: fs_cfg,
+            worker_thread: None,
+            worker_stopfd: EventFd::new(EFD_NONBLOCK).map_err(FsError::EventFd)?,
         })
     }
 
@@ -116,72 +111,6 @@ impl Fs {
 
     pub fn set_shm_region(&mut self, shm_region: VirtioShmRegion) {
         self.shm_region = Some(shm_region);
-    }
-
-    /// Signal the guest driver that we've used some virtio buffers that it had previously made
-    /// available.
-    pub fn signal_used_queue(&self) -> result::Result<(), DeviceError> {
-        debug!("fs: raising IRQ");
-        self.interrupt_status
-            .fetch_or(VIRTIO_MMIO_INT_VRING as usize, Ordering::SeqCst);
-        if let Some(intc) = &self.intc {
-            intc.lock().unwrap().set_irq(self.irq_line.unwrap());
-            Ok(())
-        } else {
-            self.interrupt_evt.write(1).map_err(|e| {
-                error!("Failed to signal used queue: {:?}", e);
-                DeviceError::FailedSignalingUsedQueue(e)
-            })
-        }
-    }
-
-    pub(crate) fn handle_hpq_event(&mut self) {
-        debug!("Fs: HPQ queue event");
-        if let Err(e) = self.queue_events[0].read() {
-            error!("Failed to get queue event: {:?}", e);
-        } else if self.process_queue(0) {
-            let _ = self.signal_used_queue();
-        }
-    }
-
-    pub(crate) fn handle_req_event(&mut self) {
-        debug!("Fs: REQ queue event");
-        if let Err(e) = self.queue_events[1].read() {
-            error!("Failed to get queue event: {:?}", e);
-        } else if self.process_queue(1) {
-            let _ = self.signal_used_queue();
-        }
-    }
-
-    pub(crate) fn process_queue(&mut self, queue_index: usize) -> bool {
-        let mem = match self.device_state {
-            DeviceState::Activated(ref mem) => mem,
-            // This should never happen, it's been already validated in the event handler.
-            DeviceState::Inactive => unreachable!(),
-        };
-
-        let queue = &mut self.queues[queue_index];
-        let mut used_any = false;
-        while let Some(head) = queue.pop(mem) {
-            let reader = Reader::new(mem, head.clone())
-                .map_err(FsError::QueueReader)
-                .unwrap();
-            let writer = Writer::new(mem, head.clone())
-                .map_err(FsError::QueueWriter)
-                .unwrap();
-
-            self.server
-                .handle_message(reader, writer, self.shm_region.as_ref())
-                //.map_err(FsError::ProcessQueue)
-                .unwrap();
-
-            if let Err(e) = queue.add_used(mem, head.index, 0) {
-                error!("failed to add used elements to the queue: {:?}", e);
-            }
-            used_any = true;
-        }
-
-        used_any
     }
 }
 
@@ -249,22 +178,33 @@ impl VirtioDevice for Fs {
     }
 
     fn activate(&mut self, mem: GuestMemoryMmap) -> ActivateResult {
-        if self.queues.len() != defs::NUM_QUEUES {
-            error!(
-                "Cannot perform activate. Expected {} queue(s), got {}",
-                defs::NUM_QUEUES,
-                self.queues.len()
-            );
-            return Err(ActivateError::BadActivate);
+        if self.worker_thread.is_some() {
+            panic!("virtio_fs: worker thread already exists");
         }
 
-        if self.activate_evt.write(1).is_err() {
-            error!("Cannot write to activate_evt",);
-            return Err(ActivateError::BadActivate);
-        }
+        let event_idx: bool = (self.acked_features & (1 << VIRTIO_RING_F_EVENT_IDX)) != 0;
+        self.queues[defs::HPQ_INDEX].set_event_idx(event_idx);
+        self.queues[defs::REQ_INDEX].set_event_idx(event_idx);
+
+        let queue_evts = self
+            .queue_events
+            .iter()
+            .map(|e| e.try_clone().unwrap())
+            .collect();
+        let worker = FsWorker::new(
+            self.queues.clone(),
+            queue_evts,
+            self.interrupt_status.clone(),
+            self.interrupt_evt.try_clone().unwrap(),
+            self.intc.clone(),
+            self.irq_line,
+            mem.clone(),
+            self.passthrough_cfg.clone(),
+            self.worker_stopfd.try_clone().unwrap(),
+        );
+        self.worker_thread = Some(worker.run());
 
         self.device_state = DeviceState::Activated(mem);
-
         Ok(())
     }
 
@@ -277,5 +217,16 @@ impl VirtioDevice for Fs {
 
     fn shm_region(&self) -> Option<&VirtioShmRegion> {
         self.shm_region.as_ref()
+    }
+
+    fn reset(&mut self) -> bool {
+        if let Some(worker) = self.worker_thread.take() {
+            let _ = self.worker_stopfd.write(1);
+            if let Err(e) = worker.join() {
+                error!("error waiting for worker thread: {:?}", e);
+            }
+        }
+        self.device_state = DeviceState::Inactive;
+        true
     }
 }

src/devices/src/virtio/fs/worker.rs
@@ -1,0 +1,171 @@
+use std::os::fd::AsRawFd;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+use utils::epoll::{ControlOperation, Epoll, EpollEvent, EventSet};
+use utils::eventfd::EventFd;
+use vm_memory::GuestMemoryMmap;
+
+use super::super::{FsError, Queue, VIRTIO_MMIO_INT_VRING};
+use super::defs::{HPQ_INDEX, REQ_INDEX};
+use super::descriptor_utils::{Reader, Writer};
+use super::passthrough::{self, PassthroughFs};
+use super::server::Server;
+use crate::legacy::Gic;
+
+pub struct FsWorker {
+    queues: Vec<Queue>,
+    queue_evts: Vec<EventFd>,
+    interrupt_status: Arc<AtomicUsize>,
+    interrupt_evt: EventFd,
+    intc: Option<Arc<Mutex<Gic>>>,
+    irq_line: Option<u32>,
+
+    mem: GuestMemoryMmap,
+    server: Server<PassthroughFs>,
+    stop_fd: EventFd,
+}
+
+impl FsWorker {
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        queues: Vec<Queue>,
+        queue_evts: Vec<EventFd>,
+        interrupt_status: Arc<AtomicUsize>,
+        interrupt_evt: EventFd,
+        intc: Option<Arc<Mutex<Gic>>>,
+        irq_line: Option<u32>,
+        mem: GuestMemoryMmap,
+        passthrough_cfg: passthrough::Config,
+        stop_fd: EventFd,
+    ) -> Self {
+        Self {
+            queues,
+            queue_evts,
+            interrupt_status,
+            interrupt_evt,
+            intc,
+            irq_line,
+
+            mem,
+            server: Server::new(PassthroughFs::new(passthrough_cfg).unwrap()),
+            stop_fd,
+        }
+    }
+
+    pub fn run(self) -> thread::JoinHandle<()> {
+        thread::spawn(|| self.work())
+    }
+
+    fn work(mut self) {
+        let virtq_hpq_ev_fd = self.queue_evts[HPQ_INDEX].as_raw_fd();
+        let virtq_req_ev_fd = self.queue_evts[REQ_INDEX].as_raw_fd();
+        let stop_ev_fd = self.stop_fd.as_raw_fd();
+
+        let epoll = Epoll::new().unwrap();
+
+        let _ = epoll.ctl(
+            ControlOperation::Add,
+            virtq_hpq_ev_fd,
+            &EpollEvent::new(EventSet::IN, virtq_hpq_ev_fd as u64),
+        );
+        let _ = epoll.ctl(
+            ControlOperation::Add,
+            virtq_req_ev_fd,
+            &EpollEvent::new(EventSet::IN, virtq_req_ev_fd as u64),
+        );
+        let _ = epoll.ctl(
+            ControlOperation::Add,
+            stop_ev_fd,
+            &EpollEvent::new(EventSet::IN, stop_ev_fd as u64),
+        );
+
+        loop {
+            let mut epoll_events = vec![EpollEvent::new(EventSet::empty(), 0); 32];
+            match epoll.wait(epoll_events.len(), -1, epoll_events.as_mut_slice()) {
+                Ok(ev_cnt) => {
+                    for event in &epoll_events[0..ev_cnt] {
+                        let source = event.fd();
+                        let event_set = event.event_set();
+                        match event_set {
+                            EventSet::IN if source == virtq_hpq_ev_fd => {
+                                self.handle_event(HPQ_INDEX);
+                            }
+                            EventSet::IN if source == virtq_req_ev_fd => {
+                                self.handle_event(REQ_INDEX);
+                            }
+                            EventSet::IN if source == stop_ev_fd => {
+                                debug!("stopping worker thread");
+                                let _ = self.stop_fd.read();
+                                return;
+                            }
+                            _ => {
+                                log::warn!(
+                                    "Received unknown event: {:?} from fd: {:?}",
+                                    event_set,
+                                    source
+                                );
+                            }
+                        }
+                    }
+                }
+                Err(e) => {
+                    debug!("failed to consume muxer epoll event: {}", e);
+                }
+            }
+        }
+    }
+
+    fn handle_event(&mut self, queue_index: usize) {
+        debug!("Fs: queue event: {}", queue_index);
+        if let Err(e) = self.queue_evts[queue_index].read() {
+            error!("Failed to get queue event: {:?}", e);
+        }
+
+        loop {
+            self.queues[queue_index]
+                .disable_notification(&self.mem)
+                .unwrap();
+
+            self.process_queue(queue_index);
+
+            if !self.queues[queue_index]
+                .enable_notification(&self.mem)
+                .unwrap()
+            {
+                break;
+            }
+        }
+    }
+
+    fn process_queue(&mut self, queue_index: usize) {
+        let queue = &mut self.queues[queue_index];
+        while let Some(head) = queue.pop(&self.mem) {
+            let reader = Reader::new(&self.mem, head.clone())
+                .map_err(FsError::QueueReader)
+                .unwrap();
+            let writer = Writer::new(&self.mem, head.clone())
+                .map_err(FsError::QueueWriter)
+                .unwrap();
+
+            if let Err(e) = self.server.handle_message(reader, writer, None) {
+                error!("error handling message: {:?}", e);
+            }
+
+            if let Err(e) = queue.add_used(&self.mem, head.index, 0) {
+                error!("failed to add used elements to the queue: {:?}", e);
+            }
+
+            if queue.needs_notification(&self.mem).unwrap() {
+                self.interrupt_status
+                    .fetch_or(VIRTIO_MMIO_INT_VRING as usize, Ordering::SeqCst);
+                if let Some(intc) = &self.intc {
+                    intc.lock().unwrap().set_irq(self.irq_line.unwrap());
+                } else if let Err(e) = self.interrupt_evt.write(1) {
+                    error!("Failed to signal used queue: {:?}", e);
+                }
+            }
+        }
+    }
+}

src/devices/src/virtio/fs/event_handler.rs
@@ -1,86 +1,0 @@
-use std::os::unix::io::AsRawFd;
-
-use polly::event_manager::{EventManager, Subscriber};
-use utils::epoll::{EpollEvent, EventSet};
-
-use super::device::{Fs, HPQ_INDEX, REQ_INDEX};
-use crate::virtio::device::VirtioDevice;
-
-impl Fs {
-    fn handle_activate_event(&self, event_manager: &mut EventManager) {
-        debug!("Fs: activate event");
-        if let Err(e) = self.activate_evt.read() {
-            error!("Failed to consume fs activate event: {:?}", e);
-        }
-
-        // The subscriber must exist as we previously registered activate_evt via
-        // `interest_list()`.
-        let self_subscriber = event_manager
-            .subscriber(self.activate_evt.as_raw_fd())
-            .unwrap();
-
-        event_manager
-            .register(
-                self.queue_events[HPQ_INDEX].as_raw_fd(),
-                EpollEvent::new(
-                    EventSet::IN,
-                    self.queue_events[HPQ_INDEX].as_raw_fd() as u64,
-                ),
-                self_subscriber.clone(),
-            )
-            .unwrap_or_else(|e| {
-                error!("Failed to register fs hpq with event manager: {:?}", e);
-            });
-
-        event_manager
-            .register(
-                self.queue_events[REQ_INDEX].as_raw_fd(),
-                EpollEvent::new(
-                    EventSet::IN,
-                    self.queue_events[REQ_INDEX].as_raw_fd() as u64,
-                ),
-                self_subscriber.clone(),
-            )
-            .unwrap_or_else(|e| {
-                error!("Failed to register fs req with event manager: {:?}", e);
-            });
-
-        event_manager
-            .unregister(self.activate_evt.as_raw_fd())
-            .unwrap_or_else(|e| {
-                error!("Failed to unregister fs activate evt: {:?}", e);
-            })
-    }
-}
-
-impl Subscriber for Fs {
-    fn process(&mut self, event: &EpollEvent, event_manager: &mut EventManager) {
-        let source = event.fd();
-        let hpq = self.queue_events[HPQ_INDEX].as_raw_fd();
-        let req = self.queue_events[REQ_INDEX].as_raw_fd();
-        let activate_evt = self.activate_evt.as_raw_fd();
-
-        if self.is_activated() {
-            match source {
-                _ if source == hpq => self.handle_hpq_event(),
-                _ if source == req => self.handle_req_event(),
-                _ if source == activate_evt => {
-                    self.handle_activate_event(event_manager);
-                }
-                _ => warn!("Unexpected fs event received: {:?}", source),
-            }
-        } else {
-            warn!(
-                "Fs: The device is not yet activated. Spurious event received: {:?}",
-                source
-            );
-        }
-    }
-
-    fn interest_list(&self) -> Vec<EpollEvent> {
-        vec![EpollEvent::new(
-            EventSet::IN,
-            self.activate_evt.as_raw_fd() as u64,
-        )]
-    }
-}

src/devices/src/virtio/fs/mod.rs
@@ -1,11 +1,11 @@
 mod device;
-mod event_handler;
 #[allow(dead_code)]
 mod filesystem;
 pub mod fuse;
 #[allow(dead_code)]
 mod multikey;
 mod server;
+mod worker;
 
 #[cfg(target_os = "linux")]
 pub mod linux;
@@ -30,10 +30,12 @@ mod defs {
     pub const FS_DEV_ID: &str = "virtio_fs";
     pub const NUM_QUEUES: usize = 2;
     pub const QUEUE_SIZES: &[u16] = &[1024; NUM_QUEUES];
+    // High priority queue.
+    pub const HPQ_INDEX: usize = 0;
+    // Request queue.
+    pub const REQ_INDEX: usize = 1;
 
     pub mod uapi {
-        /// The device conforms to the virtio spec version 1.0.
-        pub const VIRTIO_F_VERSION_1: u32 = 32;
         pub const VIRTIO_ID_FS: u32 = 26;
     }
 }

src/vmm/src/builder.rs
@@ -596,15 +596,9 @@ pub fn build_microvm(
         )?;
     }
     #[cfg(not(feature = "tee"))]
-    attach_fs_devices(
-        &mut vmm,
-        &vm_resources.fs,
-        event_manager,
-        None,
-        intc.clone(),
-    )?;
+    attach_fs_devices(&mut vmm, &vm_resources.fs, None, intc.clone())?;
     #[cfg(feature = "blk")]
-    attach_block_devices(&mut vmm, &vm_resources.block, event_manager, intc.clone())?;
+    attach_block_devices(&mut vmm, &vm_resources.block, intc.clone())?;
     if let Some(vsock) = vm_resources.vsock.get() {
         attach_unixsock_vsock_device(&mut vmm, vsock, event_manager, intc.clone())?;
         vmm.kernel_cmdline.insert_str("tsi_hijack")?;
@@ -1095,7 +1089,6 @@ fn attach_mmio_device(
 fn attach_fs_devices(
     vmm: &mut Vmm,
     fs_devs: &FsBuilder,
-    event_manager: &mut EventManager,
     shm_region: Option<VirtioShmRegion>,
     intc: Option<Arc<Mutex<Gic>>>,
 ) -> std::result::Result<(), StartMicrovmError> {
@@ -1111,10 +1104,6 @@ fn attach_fs_devices(
         if let Some(ref shm) = shm_region {
             fs.lock().unwrap().set_shm_region(shm.clone());
         }
-
-        event_manager
-            .add_subscriber(fs.clone())
-            .map_err(RegisterEvent)?;
 
         // The device mutex mustn't be locked here otherwise it will deadlock.
         attach_mmio_device(
@@ -1314,7 +1303,6 @@ fn attach_balloon_device(
 fn attach_block_devices(
     vmm: &mut Vmm,
     block_devs: &BlockBuilder,
-    event_manager: &mut EventManager,
     intc: Option<Arc<Mutex<Gic>>>,
 ) -> std::result::Result<(), StartMicrovmError> {
     use self::StartMicrovmError::*;
@@ -1325,10 +1313,6 @@ fn attach_block_devices(
         if let Some(ref intc) = intc {
             block.lock().unwrap().set_intc(intc.clone());
         }
-
-        event_manager
-            .add_subscriber(block.clone())
-            .map_err(RegisterEvent)?;
 
         // The device mutex mustn't be locked here otherwise it will deadlock.
         attach_mmio_device(

commit f9b48d710fc487c1aff5bdc86b8a3ce23b42c8b2

virtio/fs: Re-add shmem region support
Commit eac94787bab2 ("virtio/fs: move work to a separate thread")
removed support for shmem regions, and therefore DAX. Pass through the
shmem region to the worker to make DAX work again.

Fixes: eac94787bab2 ("virtio/fs: move work to a separate thread")
Signed-off-by: Asahi Lina <lina@asahilina.net>

src/devices/src/virtio/fs/device.rs
@@ -201,6 +201,7 @@ impl VirtioDevice for Fs {
             mem.clone(),
             self.passthrough_cfg.clone(),
             self.worker_stopfd.try_clone().unwrap(),
+            self.shm_region.clone(),
         );
         self.worker_thread = Some(worker.run());
 

src/devices/src/virtio/fs/worker.rs
@@ -13,6 +13,7 @@ use super::descriptor_utils::{Reader, Writer};
 use super::passthrough::{self, PassthroughFs};
 use super::server::Server;
 use crate::legacy::Gic;
+use crate::virtio::VirtioShmRegion;
 
 pub struct FsWorker {
     queues: Vec<Queue>,
@@ -25,6 +26,7 @@ pub struct FsWorker {
     mem: GuestMemoryMmap,
     server: Server<PassthroughFs>,
     stop_fd: EventFd,
+    shm_region: Option<VirtioShmRegion>,
 }
 
 impl FsWorker {
@@ -39,6 +41,7 @@ impl FsWorker {
         mem: GuestMemoryMmap,
         passthrough_cfg: passthrough::Config,
         stop_fd: EventFd,
+        shm_region: Option<VirtioShmRegion>,
     ) -> Self {
         Self {
             queues,
@@ -51,6 +54,7 @@ impl FsWorker {
             mem,
             server: Server::new(PassthroughFs::new(passthrough_cfg).unwrap()),
             stop_fd,
+            shm_region,
         }
     }
 
@@ -149,7 +153,10 @@ impl FsWorker {
                 .map_err(FsError::QueueWriter)
                 .unwrap();
 
-            if let Err(e) = self.server.handle_message(reader, writer, None) {
+            if let Err(e) = self
+                .server
+                .handle_message(reader, writer, self.shm_region.as_ref())
+            {
                 error!("error handling message: {:?}", e);
             }
 