4da847c246d4156498f4c0b8f33cf30db93d5ff0
Jake Correnti <jakecorrenti+github@proton.me>
2024-09-17 10:32:37 -0400

    vmm: initialize TDX VM
    Retrieve the TDX capabilities KVM supports with the current TDX module
    the system has loaded and perform TDX specific VM initialization.
    
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- src/vmm/src/linux/tee/inteltdx.rs
+++ src/vmm/src/linux/tee/inteltdx.rs
@@ -1,7 +1,29 @@
+use kvm_ioctls::VmFd;
+use tdx::launch::{self, Launcher};
+
+use std::os::unix::io::AsRawFd;
+
+#[derive(Debug)]
+pub enum Error {
+    GetCapabilities(launch::Error),
+    InitVm(launch::Error),
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
 pub struct IntelTdx {}
 
 impl IntelTdx {
     pub fn new() -> Self {
         Self {}
+    }
+
+    pub fn vm_prepare(&self, vm_fd: &VmFd, cpuid: kvm_bindings::CpuId) -> Result<Launcher> {
+        let mut launcher = Launcher::new(vm_fd.as_raw_fd());
+        let caps = launcher
+            .get_capabilities()
+            .map_err(Error::GetCapabilities)?;
+        launcher.init_vm(&caps, cpuid).map_err(Error::InitVm)?;
+        Ok(launcher)
     }
 }
