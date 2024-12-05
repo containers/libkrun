ce5eac8d352c9e924ac1e6127dc358bedddb9f8e
Sergio Lopez <slp@redhat.com>
2024-12-05 19:26:23 +0100

    Remove the remaning unneeded lifetimes
    clippy has started complainig about unneeded lifetimes, so remove
    them.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- src/devices/src/virtio/fs/filesystem.rs
+++ src/devices/src/virtio/fs/filesystem.rs
@@ -198,7 +198,7 @@ pub trait ZeroCopyReader {
     }
 }
 
-impl<'a, R: ZeroCopyReader> ZeroCopyReader for &'a mut R {
+impl<R: ZeroCopyReader> ZeroCopyReader for &mut R {
     fn read_to(&mut self, f: &File, count: usize, off: u64) -> io::Result<usize> {
         (**self).read_to(f, count, off)
     }
@@ -291,7 +291,7 @@ pub trait ZeroCopyWriter {
     }
 }
 
-impl<'a, W: ZeroCopyWriter> ZeroCopyWriter for &'a mut W {
+impl<W: ZeroCopyWriter> ZeroCopyWriter for &mut W {
     fn write_from(&mut self, f: &File, count: usize, off: u64) -> io::Result<usize> {
         (**self).write_from(f, count, off)
     }
