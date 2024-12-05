587c49d30ffe82fe3d4bb35bd1250f560531ade7
Sergio Lopez <slp@redhat.com>
2024-12-05 20:17:48 +0100

    vmm/boot_source: remove obsolete doc comments
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- src/vmm/src/vmm_config/boot_source.rs
+++ src/vmm/src/vmm_config/boot_source.rs
@@ -3,32 +3,15 @@
 
 use std::fmt::{Display, Formatter, Result};
 
-/// Default guest kernel command line:
-/// - `reboot=k` shut down the guest on reboot, instead of well... rebooting;
-/// - `panic=1` on panic, reboot after 1 second;
-/// - `pci=off` do not scan for PCI devices (save boot time);
-/// - `nomodule` disable loadable kernel module support;
-/// - `8250.nr_uarts=0` disable 8250 serial interface;
-/// - `i8042.noaux` do not probe the i8042 controller for an attached mouse (save boot time);
-/// - `i8042.nomux` do not probe i8042 for a multiplexing controller (save boot time);
-/// - `i8042.nopnp` do not use ACPIPnP to discover KBD/AUX controllers (save boot time);
-/// - `i8042.dumbkbd` do not attempt to control kbd state via the i8042 (save boot time).
-//pub const DEFAULT_KERNEL_CMDLINE: &str = "reboot=k panic=1 pci=off nomodule 8250.nr_uarts=0 \
-//                                          i8042.noaux i8042.nomux i8042.nopnp i8042.dumbkbd";
-
 #[cfg(all(target_os = "linux", not(feature = "tee")))]
 pub const DEFAULT_KERNEL_CMDLINE: &str = "reboot=k panic=-1 panic_print=0 nomodule console=hvc0 \
                                           rootfstype=virtiofs rw quiet no-kvmapf";
-
 #[cfg(feature = "amd-sev")]
 pub const DEFAULT_KERNEL_CMDLINE: &str = "reboot=k panic=-1 panic_print=0 nomodule console=hvc0 \
                                           root=/dev/vda rw quiet no-kvmapf";
 #[cfg(target_os = "macos")]
 pub const DEFAULT_KERNEL_CMDLINE: &str = "reboot=k panic=-1 panic_print=0 nomodule console=hvc0 \
                                           rootfstype=virtiofs rw quiet no-kvmapf";
-
-//pub const DEFAULT_KERNEL_CMDLINE: &str = "reboot=k panic=1 pci=off nomodule earlyprintk=ttyS0 \
-//                                          console=ttyS0";
 
 /// Strongly typed data structure used to configure the boot source of the
 /// microvm.
