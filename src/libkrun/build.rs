fc70f10606ed621324ddf98466dfe63d91b74209
Sergio Lopez <slp@redhat.com>
2025-01-21 14:17:33 +0100

    libkrun: load libkrunfw as a dynamic library
    Instead of linking statically against libkrunfw, load it as a dynamic
    library. This will enable us to make its presence optional, which will
    come handy when we support loading external payloads.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- src/libkrun/build.rs
+++ src/libkrun/build.rs
@@ -1,10 +1,4 @@
 fn main() {
     #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-lib=framework=Hypervisor");
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-search=/opt/homebrew/lib");
-    #[cfg(all(not(feature = "tee"), not(feature = "efi")))]
-    println!("cargo:rustc-link-lib=krunfw");
-    #[cfg(feature = "tee")]
-    println!("cargo:rustc-link-lib=krunfw-sev");
 }
