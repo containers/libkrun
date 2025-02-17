5dcc0e9badbc3ffcd07214c71f8ae18db6394cb5
Matej Hrica <mhrica@redhat.com>
2025-02-17 13:19:25 +0100

    tests: Run tests in a new network namespace if possible
    Attempt to run the tests in a new network namespace to allow the tests to use
    fixed TCP ports.
    
    Signed-off-by: Matej Hrica <mhrica@redhat.com>

--- tests/run.sh
+++ tests/run.sh
@@ -12,4 +12,15 @@ cargo test -p test_cases --features guest
 GUEST_TARGET_ARCH="$(uname -m)-unknown-linux-musl"
 
 cargo build --target=$GUEST_TARGET_ARCH -p guest-agent
-KRUN_TEST_GUEST_AGENT_PATH="target/$GUEST_TARGET_ARCH/debug/guest-agent" cargo run -p runner "$@"
+cargo build -p runner
+
+export KRUN_TEST_GUEST_AGENT_PATH="target/$GUEST_TARGET_ARCH/debug/guest-agent"
+
+if which unshare 2>&1 >/dev/null; then
+	unshare --user --map-root-user --net -- /bin/sh -c "ifconfig lo 127.0.0.1 && exec target/debug/runner $@"
+else
+	echo "WARNING: Running tests without a network namespace."
+	echo "Tests may fail if the required network ports are already in use."
+	echo
+	target/debug/runner $@
+fi
