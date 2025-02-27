26b9b080bd2c1cbb456b02370f27403b7d3eef2d
Jake Correnti <jakecorrenti+github@proton.me>
2025-02-27 14:45:03 -0500

    init tdx vcpu
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- src/vmm/src/linux/vstate.rs
+++ src/vmm/src/linux/vstate.rs
@@ -132,6 +132,9 @@ pub enum Error {
     #[cfg(feature = "intel-tdx")]
     /// Error preparing the VM for Trust Domain Extensions (TDX)
     TdxSecVirtPrepare(TdxError),
+    #[cfg(feature = "intel-tdx")]
+    /// Error initializing vCPU for Trust Domain Extensions (TDX)
+    TdxSecVirtInitVcpu,
     #[cfg(feature = "tee")]
     /// The TEE specified is not supported.
     InvalidTee,
@@ -305,6 +308,11 @@ impl Display for Error {
             TdxSecVirtPrepare(e) => write!(
                 f,
                 "Error preparing the VM for Trust Domain Extensions (TDX): {e:?}"
+            ),
+            #[cfg(feature = "intel-tdx")]
+            TdxSecVirtInitVcpu => write!(
+                f,
+                "Error initializing vCPU for Trust Domain Extensions (TDX)"
             ),
             #[cfg(feature = "tee")]
             MissingTeeConfig => write!(f, "Missing TEE configuration"),
@@ -1404,6 +1412,11 @@ impl Vcpu {
         barrier.wait();
 
         StateMachine::finish()
+    }
+
+    #[cfg(feature = "intel-tdx")]
+    pub fn tdx_secure_virt_init(&self) -> Result<()> {
+        tdx::launch::TdxVcpu::init(&self.fd, 0).or_else(|_| return Err(Error::TdxSecVirtInitVcpu))
     }
 
     #[cfg(test)]
