fe1dadfc721dc87dd51a092eecbf681b344b963e
Jake Correnti <jakecorrenti+github@proton.me>
2025-02-27 17:06:04 -0500

    add serial device to TDX or else it hangs
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- src/vmm/src/builder.rs
+++ src/vmm/src/builder.rs
@@ -510,13 +510,13 @@ pub fn build_microvm(
 
     // On x86_64 always create a serial device,
     // while on aarch64 only create it if 'console=' is specified in the boot args.
-    let serial_device = if cfg!(feature = "efi") {
+    let serial_device = if cfg!(any(feature = "efi", feature = "intel-tdx")) {
         Some(setup_serial_device(
             event_manager,
             None,
-            None,
+            // None,
             // Uncomment this to get EFI output when debugging EDK2.
-            // Some(Box::new(io::stdout())),
+            Some(Box::new(io::stdout())),
         )?)
     } else {
         None
