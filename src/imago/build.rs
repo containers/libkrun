d8c595d949aaa8759523ba809a5182af8d8748b7
Jake Correnti <jakecorrenti+github@proton.me>
2025-03-12 21:57:57 -0400

    Remove vendor-ed imago crate
    Remove the vendor-ed `imago` crate. Now that `imago` is packaged in
    Fedora, we can just add the depency through Cargo.
    
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- src/imago/build.rs
+++
@@ -1,9 +0,0 @@
-use rustc_version::{version_meta, Channel};
-
-fn main() {
-    println!("cargo:rustc-check-cfg=cfg(nightly)");
-
-    if version_meta().unwrap().channel == Channel::Nightly {
-        println!("cargo:rustc-cfg=nightly");
-    }
-}
