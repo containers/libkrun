c9e81180204b64c8ba117c729e2ee026672a9495
Tyler Fanelli <tfanelli@redhat.com>
2025-04-30 21:57:12 -0400

    nitro: Run preliminary (unconfigurable) enclave
    Uses the nitro-enclaves crate to create and run an enclave in debug mode.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

---
+++ src/nitro/src/error.rs
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use nitro_enclaves::launch::LaunchError;
+use std::{fmt, io};
+
+#[derive(Debug)]
+pub enum NitroError {
+    DeviceOpen(io::Error),
+    VmCreate(LaunchError),
+    VmMemorySet(LaunchError),
+    VcpuAdd(LaunchError),
+    HeartbeatAccept(io::Error),
+    HeartbeatBind(io::Error),
+    HeartbeatRead(io::Error),
+    HeartbeatWrite(io::Error),
+    VmStart(LaunchError),
+    PollTimeoutCalculate(LaunchError),
+    PollNoSelectedEvents,
+    PollMoreThanOneSelectedEvent,
+    EnclaveHeartbeatNotDetected,
+    HeartbeatCidMismatch,
+    VsockCreate,
+    VsockSetTimeout,
+    VsockConnect,
+}
+
+impl fmt::Display for NitroError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let msg = match self {
+            NitroError::DeviceOpen(e) => format!("unable to open nitro enclaves device: {e}"),
+            NitroError::VmCreate(e) => format!("unable to create enclave VM: {e}"),
+            NitroError::VmMemorySet(e) => format!("unable to set enclave memory regions: {e}"),
+            NitroError::VcpuAdd(e) => format!("unable to add vCPU to enclave: {e}"),
+            NitroError::HeartbeatAccept(e) => {
+                format!("unable to accept enclave heartbeat vsock: {e}")
+            }
+            NitroError::HeartbeatBind(e) => {
+                format!("unable to bind to enclave hearbeat vsock: {e}")
+            }
+            NitroError::HeartbeatRead(e) => format!("unable to read enclave hearbeat vsock: {e}"),
+            NitroError::HeartbeatWrite(e) => {
+                format!("unable to write to enclave heartbeat vsock: {e}")
+            }
+            NitroError::VmStart(e) => format!("unable to start enclave: {e}"),
+            NitroError::PollTimeoutCalculate(e) => {
+                format!("unable to calculate vsock poll timeout: {e}")
+            }
+            NitroError::PollNoSelectedEvents => {
+                "no selected poll fds for heartbeat vsock found".to_string()
+            }
+            NitroError::PollMoreThanOneSelectedEvent => {
+                "more than one selected pollfd for heartbeat vsock found".to_string()
+            }
+            NitroError::EnclaveHeartbeatNotDetected => {
+                "enclave heartbeat message not detected".to_string()
+            }
+            NitroError::HeartbeatCidMismatch => "enclave heartbeat vsock CID mismatch".to_string(),
+            NitroError::VsockCreate => "unable to create enclave vsock".to_string(),
+            NitroError::VsockSetTimeout => {
+                "unable to set poll timeout for enclave vsock".to_string()
+            }
+            NitroError::VsockConnect => "unable to connect to enclave vsock".to_string(),
+        };
+
+        write!(f, "{}", msg)
+    }
+}
