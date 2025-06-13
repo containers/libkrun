b317bdcd5892ee152762a90564d0f552e2df568f
Anders F Björklund <anders.f.bjorklund@gmail.com>
2025-06-13 19:02:48 +0200

    Add the dylib linker flags for macOS
    Signed-off-by: Anders F Björklund <anders.f.bjorklund@gmail.com>

--- src/libkrun/build.rs
+++ src/libkrun/build.rs
@@ -5,5 +5,11 @@ fn main() {
         std::env::var("CARGO_PKG_VERSION_MAJOR").unwrap()
     );
     #[cfg(target_os = "macos")]
+    println!(
+        "cargo:rustc-cdylib-link-arg=-Wl,-install_name,libkrun.{}.dylib,-compatibility_version,{}.0.0,-current_version,{}.{}.0",
+        std::env::var("CARGO_PKG_VERSION_MAJOR").unwrap(), std::env::var("CARGO_PKG_VERSION_MAJOR").unwrap(),
+        std::env::var("CARGO_PKG_VERSION_MAJOR").unwrap(), std::env::var("CARGO_PKG_VERSION_MINOR").unwrap()
+    );
+    #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-lib=framework=Hypervisor");
 }
