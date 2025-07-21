a4e0b58600adf089f4cb54e145127e4febd8cbb1
Matej Hrica <mhrica@redhat.com>
2025-07-21 17:04:09 +0200

    Add examples/krun_gtk_display reference implementation crate
    Add a reference implementation of the new display API using GTK, consumable as a
    rust crate. Eventually we may also publish this on crates.io
    
    Signed-off-by: Matej Hrica <mhrica@redhat.com>

---
+++ examples/krun_gtk_display/src/lib.rs
@@ -0,0 +1,43 @@
+mod display_backend;
+mod display_worker;
+mod scanout_paintable;
+
+use crate::display_worker::DisplayWorker;
+use anyhow::Context;
+pub use display_backend::DisplayEvent;
+pub use display_backend::GtkDisplayBackend;
+use krun_display::{DisplayBackend, IntoDisplayBackend};
+use utils::pollable_channel::{PollableChannelReciever, PollableChannelSender, pollable_channel};
+
+pub struct DisplayBackendHandle {
+    tx: PollableChannelSender<DisplayEvent>,
+}
+
+impl DisplayBackendHandle {
+    pub fn get(&self) -> DisplayBackend {
+        GtkDisplayBackend::into_display_backend(Some(&self.tx))
+    }
+}
+
+pub struct DisplayBackendWorker {
+    app_name: String,
+    rx: PollableChannelReciever<DisplayEvent>,
+}
+
+impl DisplayBackendWorker {
+    /// NOTE: on macOS GTK has to run on the main thread of the application.
+    pub fn run(self) {
+        DisplayWorker::run(self.app_name, self.rx)
+    }
+}
+
+pub fn crate_display(app_name: String) -> (DisplayBackendHandle, DisplayBackendWorker) {
+    let (tx, rx) = pollable_channel()
+        .context("Failed to create channel")
+        .unwrap();
+
+    (
+        DisplayBackendHandle { tx },
+        DisplayBackendWorker { app_name, rx },
+    )
+}
