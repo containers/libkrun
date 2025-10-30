7742a392335bc5a68cc6c8f3458b49e0d74f893e
Tyler Fanelli <tfanelli@redhat.com>
2025-10-30 00:06:34 -0400

    init/nitro: Initialize enclave filesystem, cgroups
    With the top-level root filesystem, initialize various file systems such
    as proc, tmp, sys, /dev/shm. Additionally, initialize cgroups within the
    enclave.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

---
+++ init/nitro/fs_init.c
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "include/fs_init.h"
+
+int
+filesystem_init()
+{
+    int ret;
+
+    ret = mount("proc", "/proc", "proc", MS_NODEV | MS_NOSUID | MS_NOEXEC,
+        NULL);
+    if (ret < 0) {
+        perror("mount /proc");
+        return -errno;
+    }
+
+    ret = symlink("/proc/self/fd", "/dev/fd");
+    if (ret < 0) {
+        perror("symlink add");
+        return -errno;
+    }
+
+    ret = symlink("/proc/self/fd/0", "/dev/stdin");
+    if (ret < 0) {
+        perror("symlink add");
+        return -errno;
+    }
+
+    ret = symlink("/proc/self/fd/1", "/dev/stdout");
+    if (ret < 0) {
+        perror("symlink add");
+        return -errno;
+    }
+
+    ret = symlink("/proc/self/fd/2", "/dev/stderr");
+    if (ret < 0) {
+        perror("symlink add");
+        return -errno;
+    }
+
+    ret = mount("tmpfs", "/run", "tmpfs", MS_NODEV | MS_NOSUID | MS_NOEXEC,
+        "mode=0755");
+    if (ret < 0) {
+        perror("mount /run");
+        return -errno;
+    }
+
+    ret = mount("tmpfs", "/tmp", "tmpfs", MS_NODEV | MS_NOSUID | MS_NOEXEC,
+        NULL);
+    if (ret < 0) {
+        perror("mount /tmp");
+        return -errno;
+    }
+
+    ret = mkdir("/dev/shm", 0755);
+    if (ret < 0) {
+        perror("mkdir /dev/shm");
+        return -errno;
+    }
+
+    ret = mount("shm", "/dev/shm", "tmpfs", MS_NODEV | MS_NOSUID | MS_NOEXEC,
+        NULL);
+    if (ret < 0) {
+        perror("mount /dev/shm");
+        return -errno;
+    }
+
+    ret = mkdir("/dev/pts", 0755);
+    if (ret < 0) {
+        perror("mkdir /dev/pts");
+        return -errno;
+    }
+
+    ret = mount("devpts", "/dev/pts", "devpts", MS_NOSUID | MS_NOEXEC,
+        NULL);
+    if (ret < 0) {
+        perror("mount /dev/pts");
+        return -errno;
+    }
+
+    ret = mount("sysfs", "/sys", "sysfs", MS_NODEV | MS_NOSUID | MS_NOEXEC,
+        NULL);
+    if (ret < 0) {
+        perror("mount /sys");
+        return -errno;
+    }
+
+    ret = mount("cgroup_root", "/sys/fs/cgroup", "tmpfs",
+                    MS_NODEV | MS_NOSUID | MS_NOEXEC, "mode=0755");
+    if (ret < 0) {
+        perror("mount /sys/fs/cgroup");
+        return -errno;
+    }
+
+    return 0;
+}
---
+++ init/nitro/include/cgroups_init.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _CGROUPS_H
+#define _CGROUPS_H
+
+int cgroups_init();
+
+#endif // _CGROUPS_H
