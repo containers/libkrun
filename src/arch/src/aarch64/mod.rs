2c4d515be50336556195ffc91fddb2983ffdbd48
Sergio Lopez <slp@redhat.com>
2025-11-28 12:42:05 +0100

    arch/aarch64: add fdt_addr to ArchMemoryInfo
    Calculate the right place to put the fdt in arch_memory_regions, save
    the value in ArchMemoryInfo, and deprecate get_fdt_addr().
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- src/arch/src/aarch64/mod.rs
+++ src/arch/src/aarch64/mod.rs
@@ -47,12 +47,14 @@ pub fn arch_memory_regions(
     let dram_size = align_upwards!(size, page_size);
     let ram_last_addr = layout::DRAM_MEM_START + (dram_size as u64);
     let shm_start_addr = ((ram_last_addr / 0x4000_0000) + 1) * 0x4000_0000;
+    let fdt_addr = ram_last_addr - layout::FDT_MAX_SIZE as u64;
 
     let info = ArchMemoryInfo {
         ram_last_addr,
         shm_start_addr,
         page_size,
-        initrd_addr: ram_last_addr - layout::FDT_MAX_SIZE as u64 - initrd_size,
+        fdt_addr,
+        initrd_addr: fdt_addr - initrd_size,
         firmware_addr: FIRMWARE_START,
     };
     let regions = if let Some(firmware_size) = firmware_size {
@@ -90,12 +92,6 @@ pub fn configure_system(
     Ok(())
 }
 
-// Auxiliary function to get the address where the device tree blob is loaded.
-pub fn get_fdt_addr(_mem: &GuestMemoryMmap) -> u64 {
-    // Put FDT at the beginning of the DRAM
-    layout::DRAM_MEM_START
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -114,20 +110,5 @@ mod tests {
         assert_eq!(1, regions.len());
         assert_eq!(GuestAddress(super::layout::DRAM_MEM_START), regions[0].0);
         assert_eq!(super::layout::DRAM_MEM_MAX_SIZE, regions[0].1 as u64);
-    }
-
-    #[test]
-    fn test_get_fdt_addr() {
-        let (_mem_info, regions) = arch_memory_regions(layout::FDT_MAX_SIZE - 0x1000, 0);
-        let mem = GuestMemoryMmap::from_ranges(&regions).expect("Cannot initialize memory");
-        assert_eq!(get_fdt_addr(&mem), layout::DRAM_MEM_START);
-
-        let (_mem_info, regions) = arch_memory_regions(layout::FDT_MAX_SIZE, 0);
-        let mem = GuestMemoryMmap::from_ranges(&regions).expect("Cannot initialize memory");
-        assert_eq!(get_fdt_addr(&mem), layout::DRAM_MEM_START);
-
-        let (_mem_info, regions) = arch_memory_regions(layout::FDT_MAX_SIZE + 0x1000, 0);
-        let mem = GuestMemoryMmap::from_ranges(&regions).expect("Cannot initialize memory");
-        assert_eq!(get_fdt_addr(&mem), 0x1000 + layout::DRAM_MEM_START);
     }
 }
