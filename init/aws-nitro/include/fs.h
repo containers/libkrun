8633a5e9b26d7162ac218388f38e93103b10a93e
Tyler Fanelli <tfanelli@redhat.com>
2026-02-02 20:36:40 -0500

    init/nitro: Create system directories before mount
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- init/aws-nitro/include/fs.h
+++ init/aws-nitro/include/fs.h
@@ -3,8 +3,8 @@
 #ifndef _FS_INIT_H
 #define _FS_INIT_H
 
-int console_init();
-int filesystem_init();
-int cgroups_init();
+int console_init(void);
+int filesystem_init(void);
+int cgroups_init(void);
 
 #endif // _FS_INIT_H
