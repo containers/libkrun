2fffaec5cfb3056f2abb9b5999f51a022c8216c3
Daniel Müller <deso@posteo.net>
2026-03-07 06:12:05 -0800

    devices: work with proper paths in build script
    We shouldn't stringify paths, because not all paths are valid UTF-8
    strings, meaning we could fail operations spuriously. Work with proper
    Rust paths to not run into such issues.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/devices/build.rs
+++ src/devices/build.rs
@@ -1,10 +1,15 @@
+use std::path::PathBuf;
+
 fn main() {
-    let init_binary_path = std::env::var("KRUN_INIT_BINARY_PATH").unwrap_or_else(|_| {
-        format!(
-            "{}/../../init/init",
-            std::env::var("CARGO_MANIFEST_DIR").unwrap()
-        )
-    });
-    println!("cargo:rustc-env=KRUN_INIT_BINARY_PATH={init_binary_path}");
+    let init_binary_path = std::env::var_os("KRUN_INIT_BINARY_PATH")
+        .map(PathBuf::from)
+        .unwrap_or_else(|| {
+            let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+            manifest_dir.join("../../init/init")
+        });
+    println!(
+        "cargo:rustc-env=KRUN_INIT_BINARY_PATH={}",
+        init_binary_path.display()
+    );
     println!("cargo:rerun-if-env-changed=KRUN_INIT_BINARY_PATH");
 }
