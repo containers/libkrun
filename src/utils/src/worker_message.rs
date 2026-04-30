bd10cc54e07d0711f5f6cb1145cbfac3b801ba06
lstocchi <lstocchi@redhat.com>
2026-04-30 17:13:54 +0200

    utils: fix Windows compilation issues in utils
    cfg-gate dependencies and implementations based on the target OS so that
    the utils crate also compile on Windows
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>

--- src/utils/src/worker_message.rs
+++ src/utils/src/worker_message.rs
@@ -7,16 +7,17 @@ pub struct MemoryProperties {
 
 #[derive(Debug)]
 pub enum WorkerMessage {
-    #[cfg(target_arch = "x86_64")]
+    #[cfg(all(target_arch = "x86_64", not(target_os = "windows")))]
     GsiRoute(
         crossbeam_channel::Sender<bool>,
         Vec<kvm_bindings::kvm_irq_routing_entry>,
     ),
-    #[cfg(target_arch = "x86_64")]
+    #[cfg(all(target_arch = "x86_64", not(target_os = "windows")))]
     IrqLine(crossbeam_channel::Sender<bool>, u32, bool),
     #[cfg(target_os = "macos")]
     GpuAddMapping(crossbeam_channel::Sender<bool>, u64, u64, u64),
     #[cfg(target_os = "macos")]
     GpuRemoveMapping(crossbeam_channel::Sender<bool>, u64, u64),
+    #[cfg(not(target_os = "windows"))]
     ConvertMemory(crossbeam_channel::Sender<bool>, MemoryProperties),
 }
